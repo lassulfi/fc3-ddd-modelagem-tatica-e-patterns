88fddc9da39b8b13b8e13c84241bd9f1e20506d6
Luis Daniel Assulfi <lassulfi@gmail.com>
2022-05-17 22:00:35 -0300

    clean-code - testando usecase para listagem de customer

---
+++ src/usercase/customer/update/update.customer.unit.spec.ts
@@ -0,0 +1,68 @@
+import CustomerFactory from "../../../domain/customer/factory/customer.factory";
+import Address from "../../../domain/customer/value-objects/address";
+import UpdateCustomerUseCase from "./update.customer.usecase";
+
+const address = new Address("Street", 123, "zip", "city");
+const customer = CustomerFactory.createWithAddress("John", address);
+
+const input = {
+    id: customer.id,
+    name: "John Updated",
+    address: {
+        street: "Street Updated",
+        number: 1234,
+        zip: "Zip Updated",
+        city: "City Updated",
+    },
+};
+
+const MockRepository = () => {
+    return {
+        create: jest.fn(),
+        findAll: jest.fn(),
+        find: jest.fn(),
+        update: jest.fn(),
+    };
+};
+
+describe("Unit test update customer use case", () => {
+    it("should update a customer", async () => {
+        const customerRepository = MockRepository();
+        const findCustomerSpy = jest.spyOn(customerRepository, 'find')
+            .mockReturnValue(Promise.resolve(customer));
+
+        const upateCustomerUseCase = new UpdateCustomerUseCase(customerRepository);
+
+        const output = await upateCustomerUseCase.execute(input);
+
+        expect(output).toStrictEqual(input);
+        expect(findCustomerSpy).toHaveBeenCalledWith(customer.id);
+    });
+
+    it("should thrown an error when name is missing", async () => {
+        const customerRepository = MockRepository();
+        const findCustomerSpy = jest.spyOn(customerRepository, 'find')
+            .mockReturnValue(Promise.resolve(customer));
+
+        const upateCustomerUseCase = new UpdateCustomerUseCase(customerRepository);
+
+        input.name = "";
+
+        await expect(upateCustomerUseCase.execute(input)).rejects.toThrow("Name is required");
+        expect(findCustomerSpy).toHaveBeenCalledWith(customer.id)
+    });
+
+    it("should thrown an error when address is missing",async () => {
+        const customerRepository = MockRepository();
+        const findCustomerSpy = jest.spyOn(customerRepository, 'find')
+            .mockReturnValue(Promise.resolve(customer));
+
+        const upateCustomerUseCase = new UpdateCustomerUseCase(customerRepository);
+
+        input.name = "John Updated";
+        input.address.street = "";
+
+        await expect(upateCustomerUseCase.execute(input)).rejects.toThrow("Street is required");
+        expect(findCustomerSpy).toHaveBeenCalledWith(customer.id) 
+    });
+});
